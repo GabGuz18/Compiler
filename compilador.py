23855dcd8509d51cfe5caaeec485ff9e4950c5bd
GabGuz18 <gugd2001@hotmail.com>
2023-04-14 18:48:19 -0600

    Condicionado para integrar

--- compilador.py
+++ compilador.py
@@ -11,37 +11,37 @@ class Compilador:
         print('Algo() -> Aqui hace algo\n')
         #Aburrido()
 
-    def instruccion(self,opciones,opc):
+    def instruccion(self,opc):
 
         print('------------------------- Nivel 1 -------------------------')
         #id = divertido
 
         instr = Word(alphas) + "=" + Word(nums)
-        comp = f"id={opciones[opc]}"
+        comp = f"id={opc}"
         
         try:
             self.id = int(instr.parseString(comp)[2])
             print(f"id = {self.id}\n")
             
         except ParseException as err:
-            print(f"id={opciones[opc]}\n")
+            print(f"id={opc}\n")
             print(" " * (err.column - 1) + "^")
             print('Error en la asignación de valores.\n')
             print('Se esperaba un número, se encontro un caracter (linea:1, columna:',err.column,')')
 
-    def instruccion2(self,opciones2,opc,opc2):
+    def instruccion2(self,opc,opc2):
 
         print('------------------------- Nivel 2 -------------------------')
 
         instr = Word(alphas) + "=" + Word(alphas) + '+' + Word(nums)
-        comp = f"id={opciones2[opc]}+{opciones2[opc2]}"
+        comp = f"id={opc}+{opc2}"
         
         try:
             self.id = self.id + int(instr.parseString(comp)[4])
-            print(f"id = {opciones2[opc]} + {opciones2[opc2]}")
+            print(f"id = {opc} + {opc2}")
             print(f"Ahora:\nid = {self.id}")
         except ParseException as err:
-            print(f"id = {opciones2[opc]} + {opciones2[opc2]}")
+            print(f"id = {opc} + {opc2}")
             print(" " * (err.column - 1) + "^")
             print('Error en la asignación de valores.\n')
             if err.column == 4:
@@ -49,18 +49,18 @@ class Compilador:
             else:
                 print('Se esperaba un numero y se recibio una variable. (linea:1, columna:',err.column,')')
 
-    def condicional(self,opciones3,opc,opc2):
+    def condicional(self,opc,opc2):
         print('------------------------- Nivel 3 -------------------------')
 
         instr = Word(alphas) + ":" + Word(alphas) + '=' + Word(nums) + Word(alphas) + ':' + Word(alphas) + '()' 
-        comp = f"{opciones3[opc]}: id={self.id} Entonces: {opciones3[opc2]}()"
-        
+        comp = f"{opc}: id={self.id} Entonces: {opc2}()"
+
         try:
-            print(f"{opciones3[opc]}: id={self.id} Entonces: {opciones3[opc2]}()")
+            print(f"{opc}: id={self.id} Entonces: {opc2}()")
             if self.id == 20:
                 self.algo()
         except ParseException as err:
-            print(f"{opciones3[opc]}: id={self.id} Entonces: {opciones3[opc2]}()")
+            print(f"{opc}: id={self.id} Entonces: {opc2}()")
             print(" " * (err.column - 1) + "^")
             print('Error en la asignación de valores.\n')
             if err.column == 1:
@@ -68,18 +68,18 @@ class Compilador:
             else:
                 print('Se esperaba una funcion y se recibio otra opción. (linea:1, columna:',err.column,')')
 
-    def ciclo(self, opciones4, opc, opc2):
+    def ciclo(self, opc, opc2):
         print('------------------------- Nivel 4 -------------------------')
 
         instr = Word(alphas) + ":" + Word(alphas) + '=' + Word(nums) + Word(alphas) + ':' + Word(alphas) + '()' 
-        comp = f"{opciones4[opc]}: id={self.id} Entonces: {opciones4[opc2]}()"
+        comp = f"{opc}: id={self.id} Entonces: {opc2}()"
         
         try:
-            print(f"{opciones4[opc]}: id={self.id} Entonces: {opciones4[opc2]}()")
+            print(f"{opc}: id={self.id} Entonces: {opc2}()")
             if self.id == 20:
                 self.algo()
         except ParseException as err:
-            print(f"{opciones4[opc]}: id={self.id} Entonces: {opciones4[opc2]}()")
+            print(f"{opc}: id={self.id} Entonces: {opc2}()")
             print(" " * (err.column - 1) + "^")
             print('Error en la asignación de valores.\n')
             if err.column == 1:
@@ -87,17 +87,17 @@ class Compilador:
             else:
                 print('Se esperaba una funcion y se recibio otra opción. (linea:1, columna:',err.column,')')
 
-    def ciclo_y_condicional(self, opciones5, opc, opc2):
+    def ciclo_y_condicional(self, opc, opc2):
         print('------------------------- Nivel 5 -------------------------')
         #mientras: aburrido() Entonces: divertido = divertido + 10 Si: divertido = 100 Entonces: regresar()  
 
         instr = Word(alphas) + ":" + Word(alphas) + '()' + Word(alphas) + ':' + Word(alphas) + '=' + Word(alphas) + "+" + Word(nums) + Word(alphas) + ":" + Word(alphas) + '=' + Word(nums) + Word(alphas) + ':' + Word(alphas) + '()'
-        comp = f"{opciones5[opc]}: Aburrido() Entonces: divertido = {opciones5[opc2]} + 10 Si: divertido = 100 Entonces: Algo()"
+        comp = f"{opc}: Aburrido() Entonces: divertido = {opc2} + 10 Si: divertido = 100 Entonces: Algo()"
         
         try:
-            print(f"{opciones5[opc]}: Aburrido() Entonces: divertido = {opciones5[opc2]} + 10 Si: divertido = 100 Entonces: Algo()")
+            print(f"{opc}: Aburrido() Entonces: divertido = {opc2} + 10 Si: divertido = 100 Entonces: Algo()")
         except ParseException as err:
-            print(f"{opciones5[opc]}: Aburrido() Entonces: divertido = {opciones5[opc2]} + 10 Si: divertido = 100 Entonces: Algo()")
+            print(f"{opc}: Aburrido() Entonces: divertido = {opc2} + 10 Si: divertido = 100 Entonces: Algo()")
             print(" " * (err.column - 1) + "^")
             print('Error en la declaracion de ciclo.\n')
             if err.column == 1:
@@ -105,14 +105,9 @@ class Compilador:
             else:
                 print('Se esperaba una variable y se recibio otra opción. (linea:1, columna:',err.column,')')
 
-opciones1 = {1:10,2:'id',3:'diez'}
-opciones2 = {1:10,2:'id',3:'diez',5:'Si:'}
-opciones3 = {1:'Si',2:'id',3:'diez',4:'x',5:10,6:'Algo'}
-opciones4 = {1:'Si',2:'id',3:'Mientras',4:'x',5:10,6:'Algo',7:'diez'}
-opciones5 = {1:'Si',2:'id',3:'Mientras',4:'x',5:10,6:'Algo',7:'diez',8:'Hasta', 9:'Aburrido'}
 
 comp = Compilador()
-comp.instruccion(opciones1,1)
-comp.instruccion2(opciones2,2,1)
-comp.condicional(opciones3,1,6)
-comp.ciclo(opciones4,3,6)
+comp.instruccion(1)
+comp.instruccion2(2,1)
+comp.condicional(1,6)
+comp.ciclo(3,6)
